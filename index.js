a1de230cccb0371745cb0ee24f11115baa2aa71b
vatsaljoshi22 <joshivatsal256@gmail.com>
2024-05-31 15:07:49 +0530

    git add unmerge

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ const age = 24;
 const name = 'vatsal';
 
 function sayname(){
+<<<<<<< HEAD
     console.log("my name is" + name)
+=======
+    console.log("my name is" + name1)
+}
+
+function ageabove18(){
+    return age >=18
+>>>>>>> parent of 376ec1c (add sum on index,js)
 }
